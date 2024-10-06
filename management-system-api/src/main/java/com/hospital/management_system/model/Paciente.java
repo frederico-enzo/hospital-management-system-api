2e6909ace14c430299aa30e3282c9c9b59a58f17
Frederico <frederico.enzo@hotmail.com>
2024-10-06 16:25:59 -0300

    feat: model paciente ok com as anotation para criaçao da tabela no banco e relacinamente com prontuario

--- management-system-api/src/main/java/com/hospital/management_system/model/Paciente.java
+++ management-system-api/src/main/java/com/hospital/management_system/model/Paciente.java
@@ -1,18 +1,50 @@
 package com.hospital.management_system.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity @Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
 @Table(name = "tb.pacientes", schema = "public")
 public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
     private Long id;
 
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "sobrenome")
+    private String sobrenome;
+
+    @Column(name = "dataNascimento")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate dataNascimento;
+
+    @Column(name = "telefone", unique = true)
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "Número de telefone deve estar no formato (XX) XXXXX-XXXX")
+    private String telefone;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "cpf", unique = true)
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "CPF deve estar no formato XXX.XXX.XXX-XX")
+    private String cpf;
+
+    @Column(name = "genero")
+    private String genero;
+
+    @OneToMany(mappedBy = "paciente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Prontuario> prontuarios;
 }
--- management-system-api/src/main/java/com/hospital/management_system/model/Prontuario.java
+++ management-system-api/src/main/java/com/hospital/management_system/model/Prontuario.java
@@ -1,4 +1,21 @@
 package com.hospital.management_system.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "tb.pacientes", schema = "public")
 public class Prontuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id", nullable = false)
+    private Paciente paciente;
 }
