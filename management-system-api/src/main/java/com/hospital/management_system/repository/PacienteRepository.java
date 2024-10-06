4fe720816c85989e043df23e6d0df05572570f75
Frederico <frederico.enzo@hotmail.com>
2024-10-06 16:34:53 -0300

    feat: paciente repository

---
+++ management-system-api/src/main/java/com/hospital/management_system/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.hospital.management_system.repository;
+
+import com.hospital.management_system.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
