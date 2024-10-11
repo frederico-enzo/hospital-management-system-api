00ce7a0987d35695d9b32184c98572e531517325
Frederico <frederico.enzo@hotmail.com>
2024-10-11 19:30:02 -0300

    feat:  modelo Relatório com as anotações do JPA, levando em consideração as funcionalidades

--- management-system-api/src/main/java/com/hospital/management_system/model/Relatorio.java
+++ management-system-api/src/main/java/com/hospital/management_system/model/Relatorio.java
@@ -1,4 +1,32 @@
 package com.hospital.management_system.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity @Data
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "tb.pacientes", schema = "public")
 public class Relatorio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_relatorio")
+    private Long id;
+
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "tipo", nullable = false, length = 50)
+    private String tipo;
+
+    @Column(name = "conteudo", nullable = false, length = 5000)
+    private String conteudo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_medico", nullable = false)
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_paciente", nullable = true)
+    private Paciente paciente;
 }
