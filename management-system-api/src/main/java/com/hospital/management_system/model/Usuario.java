8dc71000189dcf5ce8a81b103d3a68651c7c55c2
Frederico <frederico.enzo@hotmail.com>
2024-10-11 20:02:21 -0300

    fix: Error creating bean with name 'usuarioRepository'

--- management-system-api/src/main/java/com/hospital/management_system/model/Usuario.java
+++ management-system-api/src/main/java/com/hospital/management_system/model/Usuario.java
@@ -1,4 +1,14 @@
 package com.hospital.management_system.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor @AllArgsConstructor @Data
+@Entity @Table(name = "tb.usuario", schema = "public")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_agenda")
+    private Long id;
+
 }
