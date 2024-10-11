89410e1092e0ad422778ba9209c75cd3e9540f49
Frederico <frederico.enzo@hotmail.com>
2024-10-11 19:27:47 -0300

    feat: modelo Prontuário com as anotações do JPA, considerando os relacionamentos com as outras entidades

--- management-system-api/src/main/java/com/hospital/management_system/model/Prontuario.java
+++ management-system-api/src/main/java/com/hospital/management_system/model/Prontuario.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 @Table(name = "tb.pacientes", schema = "public")
@@ -13,9 +15,29 @@ public class Prontuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_prontuario")
     private Long id;
 
+    @Column(name = "data_criacao", nullable = false)
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "historico_medico", nullable = false, length = 2000)
+    private String historicoMedico;
+
+    @Column(name = "diagnosticos", length = 2000)
+    private String diagnosticos;
+
+    @Column(name = "tratamentos", length = 2000)
+    private String tratamentos;
+
+    @Column(name = "medicacoes", length = 2000)
+    private String medicacoes;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "paciente_id")
+    @JoinColumn(name = "id_paciente", nullable = false)
     private Paciente paciente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_medico", nullable = false)
+    private Medico medico;
 }
