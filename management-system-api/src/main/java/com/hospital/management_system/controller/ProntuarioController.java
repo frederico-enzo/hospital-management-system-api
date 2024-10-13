078139a209cad17ad193130d8ede63ce66f648f7
Frederico <frederico.enzo@hotmail.com>
2024-10-12 23:00:03 -0300

    create: prontuario controller

---
+++ management-system-api/src/main/java/com/hospital/management_system/controller/ProntuarioController.java
@@ -0,0 +1,9 @@
+package com.hospital.management_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/prontuario")
+public class ProntuarioController {
+}
