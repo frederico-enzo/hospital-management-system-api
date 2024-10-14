67027e2ea7cfc958f8645199140eff66ed8e34ef
Frederico <frederico.enzo@hotmail.com>
2024-10-14 19:03:31 -0300

    feat: prontuario dto

--- management-system-api/src/main/java/com/hospital/management_system/dto/ProntuarioDto.java
+++ management-system-api/src/main/java/com/hospital/management_system/dto/ProntuarioDto.java
@@ -1,17 +1,34 @@
 package com.hospital.management_system.dto;
 
+import com.hospital.management_system.model.Consulta;
+import com.hospital.management_system.model.Medico;
 import com.hospital.management_system.model.Paciente;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProntuarioDto {
     private Long id;
+
+    private LocalDateTime dataCriacao;
+
+    private String historicoMedico;
+
+    private String diagnosticos;
+
+    private String tratamentos;
+
+    private String medicacoes;
+
     private Paciente paciente;
-    private List<String> historicoMedico;
-    private List<String> tratamentos;
+
+    private Medico medico;
+
+    private Consulta consulta;
 }
