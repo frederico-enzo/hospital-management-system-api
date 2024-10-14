e79ec205b93c24fc8575041fe1bd5803319e1844
Frederico <frederico.enzo@hotmail.com>
2024-10-14 18:54:17 -0300

    feat: agenda dto

--- management-system-api/src/main/java/com/hospital/management_system/dto/AgendaDto.java
+++ management-system-api/src/main/java/com/hospital/management_system/dto/AgendaDto.java
@@ -1,24 +1,24 @@
 package com.hospital.management_system.dto;
 
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import com.hospital.management_system.model.Consulta;
+import com.hospital.management_system.model.Medico;
+import com.hospital.management_system.model.Paciente;
 import lombok.*;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
- @Data
-@NoArgsConstructor @AllArgsConstructor
+
+@NoArgsConstructor @AllArgsConstructor  @Data
 public class AgendaDto {
-        private Long id;
+         private Long id;
 
-        @NotNull(message = "O horário disponível não pode ser nulo")
-        private LocalDateTime horarioDisponivel;
-        @NotNull(message = "O campo 'disponível' deve ser especificado")
-        private boolean disponivel;
-        @NotNull(message = "O médico não pode ser nulo")
-        private MedicoDto medico;
-        @NotEmpty(message = "A lista de consultas não pode estar vazia")
-        private List<ConsultaDto> consultas;
+         private LocalDateTime dataHora;
+
+         private String status;
+
+         private Paciente paciente;
+
+         private Medico medico;
+
+         private Consulta consulta;
 }
