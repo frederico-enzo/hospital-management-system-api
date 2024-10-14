71beb0f2b20b827cefbef6ba8c955ed0d8de144e
Frederico <frederico.enzo@hotmail.com>
2024-10-14 19:00:35 -0300

    feat: consulta dto

--- management-system-api/src/main/java/com/hospital/management_system/dto/ConsultaDto.java
+++ management-system-api/src/main/java/com/hospital/management_system/dto/ConsultaDto.java
@@ -3,20 +3,31 @@ package com.hospital.management_system.dto;
 import com.hospital.management_system.model.Agenda;
 import com.hospital.management_system.model.Medico;
 import com.hospital.management_system.model.Paciente;
+import com.hospital.management_system.model.Prontuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConsultaDto {
     private Long id;
+
+    private LocalDateTime dataHora;
+
+    private String motivo;
+
+    private String observacoes;
+
     private Paciente paciente;
+
     private Medico medico;
-    private String dataConsulta;
-    private String horaConsulta;
-    private String status;
+
+    private Prontuario prontuario;
+
     private Agenda agenda;
 
 }
