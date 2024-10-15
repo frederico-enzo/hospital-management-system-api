39136ff3173ba1c05aa22bd555559737e35103d8
Frederico <frederico.enzo@hotmail.com>
2024-10-15 16:12:43 -0300

    fix: paciente service

--- management-system-api/src/main/java/com/hospital/management_system/service/PacienteService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/PacienteService.java
@@ -2,9 +2,7 @@ package com.hospital.management_system.service;
 
 import com.hospital.management_system.dto.PacienteDto;
 import com.hospital.management_system.model.Paciente;
-import com.hospital.management_system.model.Prontuario;
 import com.hospital.management_system.repository.PacienteRepository;
-import com.hospital.management_system.repository.ProntuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +11,6 @@ import org.springframework.stereotype.Service;
 public class PacienteService{
     @Autowired
     private PacienteRepository repository;
-    @Autowired
-    private ProntuarioRepository prontuarioRepository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -26,18 +22,6 @@ public class PacienteService{
     }
 
 
-    public PacienteDto get(Long id){
-        final Paciente paciente = repository.findById(id).orElse(null);
-        return toDTO(paciente);
-    }
-    public PacienteDto post(PacienteDto pacienteDto){
-        return toDTO(repository.save(toModel(pacienteDto)));
-    }
-    public PacienteDto update(PacienteDto pacienteDto, Long id){
-        return toDTO(repository.save(toModel(pacienteDto)));
-    }
-    public void delete(Long id){
-        repository.deleteById(id);
-    }
+
 
 }
