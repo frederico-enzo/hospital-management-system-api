f95d6de1a02e1af25127006b53ca0b6005be0065
Frederico <frederico.enzo@hotmail.com>
2024-10-16 23:43:36 -0300

    feat: modele mappar convertedor de dto para model e model para dto para : Relatorio

--- management-system-api/src/main/java/com/hospital/management_system/service/RelatorioService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/RelatorioService.java
@@ -1,4 +1,22 @@
 package com.hospital.management_system.service;
 
+import com.hospital.management_system.dto.ProntuarioDto;
+import com.hospital.management_system.dto.RelatorioDto;
+import com.hospital.management_system.model.Prontuario;
+import com.hospital.management_system.model.Relatorio;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RelatorioService {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Relatorio toModel(RelatorioDto relatorioDto){
+        return modelMapper.map(relatorioDto, Relatorio.class);
+    }
+    private RelatorioDto toDTO(Relatorio relatorio){
+        return modelMapper.map(relatorio, RelatorioDto.class);
+    }
 }
