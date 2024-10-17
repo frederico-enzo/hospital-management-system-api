63fa8a7254a05e723349be25fd724ac38a8ba188
Frederico <frederico.enzo@hotmail.com>
2024-10-16 23:42:34 -0300

    feat: modele mappar convertedor de dto para model e model para dto para : Prontuario

--- management-system-api/src/main/java/com/hospital/management_system/service/ProntuarioService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/ProntuarioService.java
@@ -1,4 +1,22 @@
 package com.hospital.management_system.service;
 
+import com.hospital.management_system.dto.MedicoDto;
+import com.hospital.management_system.dto.ProntuarioDto;
+import com.hospital.management_system.model.Medico;
+import com.hospital.management_system.model.Prontuario;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProntuarioService {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Prontuario toModel(ProntuarioDto prontuarioDto){
+        return modelMapper.map(prontuarioDto, Prontuario.class);
+    }
+    private ProntuarioDto toDTO(Prontuario prontuario){
+        return modelMapper.map(prontuario, ProntuarioDto.class);
+    }
 }
