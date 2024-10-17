5946ea29edb2851738d4630fe25cd80690d6948c
Frederico <frederico.enzo@hotmail.com>
2024-10-16 23:39:28 -0300

    feat: modele mappar convertedor de dto para model e model para dto para : Agenda

--- management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
@@ -1,4 +1,22 @@
 package com.hospital.management_system.service;
 
+import com.hospital.management_system.dto.AgendaDto;
+import com.hospital.management_system.dto.PacienteDto;
+import com.hospital.management_system.model.Agenda;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AgendaService {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Agenda toModel(AgendaDto agendaDto){
+        return modelMapper.map(agendaDto, Agenda.class);
+    }
+    private AgendaDto toDTO(Agenda agenda){
+        return modelMapper.map(agenda, AgendaDto.class);
+    }
+
 }
