b95a993efcb2ada008361939f6416869d2377675
Frederico <frederico.enzo@hotmail.com>
2024-10-16 23:40:26 -0300

    feat: modele mappar convertedor de dto para model e model para dto para : Consulta

--- management-system-api/src/main/java/com/hospital/management_system/service/ConsultaService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/ConsultaService.java
@@ -1,4 +1,22 @@
 package com.hospital.management_system.service;
 
+import com.hospital.management_system.dto.AgendaDto;
+import com.hospital.management_system.dto.ConsultaDto;
+import com.hospital.management_system.model.Agenda;
+import com.hospital.management_system.model.Consulta;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConsultaService {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Consulta toModel(ConsultaDto consultaDto){
+        return modelMapper.map(consultaDto, Consulta.class);
+    }
+    private ConsultaDto toDTO(Consulta consulta){
+        return modelMapper.map(consulta, ConsultaDto.class);
+    }
 }
