710a5aafdf5ab66e099ec720c4b27992062c27d5
Frederico <frederico.enzo@hotmail.com>
2024-10-18 13:44:05 -0300

    fix: mudei o request mapping que esta igual a do prontuario

--- management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/prontuario")
+@RequestMapping(value = "/api/relatorio")
 public class RelatorioController {
     @Autowired
     private ProntuarioService service;
