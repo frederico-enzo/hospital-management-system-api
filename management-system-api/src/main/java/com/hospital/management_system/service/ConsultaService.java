3f4dc90cec9fdd6d5d8265b97c2213a775ad938f
Frederico <frederico.enzo@hotmail.com>
2024-10-18 14:15:40 -0300

    feat: metodos create-updade para service de Consulta

--- management-system-api/src/main/java/com/hospital/management_system/service/ConsultaService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/ConsultaService.java
@@ -4,12 +4,15 @@ import com.hospital.management_system.dto.AgendaDto;
 import com.hospital.management_system.dto.ConsultaDto;
 import com.hospital.management_system.model.Agenda;
 import com.hospital.management_system.model.Consulta;
+import com.hospital.management_system.repository.ConsultaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsultaService {
+    @Autowired
+    private ConsultaRepository repository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -18,5 +21,11 @@ public class ConsultaService {
     }
     private ConsultaDto toDTO(Consulta consulta){
         return modelMapper.map(consulta, ConsultaDto.class);
+    }
+    public ConsultaDto create(ConsultaDto model){
+        return toDTO(repository.save(toModel(model)));
+    }
+    public ConsultaDto update(ConsultaDto model){
+        return toDTO(repository.save(toModel(model)));
     }
 }
