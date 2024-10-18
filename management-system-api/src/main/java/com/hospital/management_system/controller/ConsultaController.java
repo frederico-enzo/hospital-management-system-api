a64d640f72616ffa82cab196b94a77fe8fac82e1
Frederico <frederico.enzo@hotmail.com>
2024-10-17 23:02:09 -0300

    feat: add get '/lista' pra fazer um find All em consulta

--- management-system-api/src/main/java/com/hospital/management_system/controller/ConsultaController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/ConsultaController.java
@@ -1,7 +1,11 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.repository.AgendaRepository;
+import com.hospital.management_system.repository.ConsultaRepository;
 import com.hospital.management_system.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsultaController {
     @Autowired
     private ConsultaService service;
+    @Autowired
+    private ConsultaRepository repository;
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> listarAll(){
+        return ResponseEntity.ok(this.repository.findAll());
+    }
 }
+
