04d222497b4e3beb0f5a376bfc5eb60597bfbf17
Frederico <frederico.enzo@hotmail.com>
2024-10-17 22:58:18 -0300

    feat : add autowired service-agenda

--- management-system-api/src/main/java/com/hospital/management_system/controller/AgendaController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/AgendaController.java
@@ -1,9 +1,13 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.service.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/agenda")
 public class AgendaController {
+    @Autowired
+    private AgendaService service;
 }
