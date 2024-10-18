d637bedec6d46e725e30e13b8a184befc5497919
Frederico <frederico.enzo@hotmail.com>
2024-10-18 14:13:50 -0300

    feat: metodos create-updade para service de Agenda

--- management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
@@ -1,14 +1,18 @@
 package com.hospital.management_system.service;
 
 import com.hospital.management_system.dto.AgendaDto;
+import com.hospital.management_system.dto.MedicoDto;
 import com.hospital.management_system.dto.PacienteDto;
 import com.hospital.management_system.model.Agenda;
+import com.hospital.management_system.repository.AgendaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendaService {
+    @Autowired
+    private AgendaRepository repository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -17,6 +21,13 @@ public class AgendaService {
     }
     private AgendaDto toDTO(Agenda agenda){
         return modelMapper.map(agenda, AgendaDto.class);
+    }
+
+    public AgendaDto create(AgendaDto model){
+        return toDTO(repository.save(toModel(model)));
+    }
+    public AgendaDto update(AgendaDto model){
+        return toDTO(repository.save(toModel(model)));
     }
 
 }
