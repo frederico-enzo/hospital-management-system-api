b60550f88ef8418f6dbc90e6d5753b856f42aafc
Frederico <frederico.enzo@hotmail.com>
2024-10-17 22:58:50 -0300

    feat : add autowired service-consulta

--- management-system-api/src/main/java/com/hospital/management_system/controller/ConsultaController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/ConsultaController.java
@@ -1,9 +1,13 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/consulta")
 public class ConsultaController {
+    @Autowired
+    private ConsultaService service;
 }
