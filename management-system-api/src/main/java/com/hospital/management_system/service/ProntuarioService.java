82ed4e9924ab88cd44f35f1749194320c16e3cb5
Frederico <frederico.enzo@hotmail.com>
2024-10-18 14:14:56 -0300

    feat: metodos create-updade para service de Prontuario

--- management-system-api/src/main/java/com/hospital/management_system/service/ProntuarioService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/ProntuarioService.java
@@ -1,15 +1,20 @@
 package com.hospital.management_system.service;
 
 import com.hospital.management_system.dto.MedicoDto;
+import com.hospital.management_system.dto.PacienteDto;
 import com.hospital.management_system.dto.ProntuarioDto;
 import com.hospital.management_system.model.Medico;
 import com.hospital.management_system.model.Prontuario;
+import com.hospital.management_system.repository.PacienteRepository;
+import com.hospital.management_system.repository.ProntuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProntuarioService {
+    @Autowired
+    private ProntuarioRepository repository;
     @Autowired
     private ModelMapper modelMapper;
 
@@ -18,5 +23,11 @@ public class ProntuarioService {
     }
     private ProntuarioDto toDTO(Prontuario prontuario){
         return modelMapper.map(prontuario, ProntuarioDto.class);
+    }
+    public ProntuarioDto create(ProntuarioDto model){
+        return toDTO(repository.save(toModel(model)));
+    }
+    public ProntuarioDto update(ProntuarioDto model){
+        return toDTO(repository.save(toModel(model)));
     }
 }
