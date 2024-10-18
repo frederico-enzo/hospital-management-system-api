09995e4cb4c97dbf27181b5747433bf996b1f3eb
Frederico <frederico.enzo@hotmail.com>
2024-10-17 23:02:30 -0300

    feat: add get '/lista' pra fazer um find All em medico

--- management-system-api/src/main/java/com/hospital/management_system/controller/MedicoController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/MedicoController.java
@@ -1,7 +1,11 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.repository.AgendaRepository;
+import com.hospital.management_system.repository.MedicoRepository;
 import com.hospital.management_system.service.ConsultaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
     @Autowired
     private ConsultaService service;
+    @Autowired
+    private MedicoRepository repository;
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> listarAll(){
+        return ResponseEntity.ok(this.repository.findAll());
+    }
 }
