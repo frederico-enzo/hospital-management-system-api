8f7bdc24cf3eef6f44014b8da3183c9a6a72d53e
Frederico <frederico.enzo@hotmail.com>
2024-10-17 23:04:46 -0300

    feat: add autowired para service-repository - make a get '/lista' para fazer um find all em Relatorio

--- management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
@@ -1,9 +1,23 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.repository.ProntuarioRepository;
+import com.hospital.management_system.service.ProntuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/prontuario")
 public class RelatorioController {
+    @Autowired
+    private ProntuarioService service;
+    @Autowired
+    private ProntuarioRepository repository;
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> listarAll(){
+        return ResponseEntity.ok(this.repository.findAll());
+    }
 }
