ff4aec0732f7fcdc6556fa85ecff5b0ab25cb854
Frederico <frederico.enzo@hotmail.com>
2024-10-17 22:59:18 -0300

    feat : add autowired service-medico

--- management-system-api/src/main/java/com/hospital/management_system/controller/MedicoController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/MedicoController.java
@@ -1,9 +1,13 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.service.ConsultaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/medico")
 public class MedicoController {
+    @Autowired
+    private ConsultaService service;
 }
