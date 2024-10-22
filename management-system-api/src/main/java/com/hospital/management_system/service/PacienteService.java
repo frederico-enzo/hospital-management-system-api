313f7654cdb523ea4713bb1a3248c4d4782c3ef5
Frederico <frederico.enzo@hotmail.com>
2024-10-22 19:32:31 -0300

    feat metodos - read e deale para service Paciente

--- management-system-api/src/main/java/com/hospital/management_system/service/PacienteService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/PacienteService.java
@@ -1,8 +1,10 @@
 package com.hospital.management_system.service;
 
 import com.hospital.management_system.dto.PacienteDto;
+import com.hospital.management_system.dto.RelatorioDto;
 import com.hospital.management_system.model.Paciente;
 import com.hospital.management_system.repository.PacienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,11 @@ public class PacienteService{
         return toDTO(repository.save(toModel(model)));
     }
 
-
+    public void delete(Long id){ repository.deleteById(id);}
+    public PacienteDto read(Long id) {
+        return repository.findById(id)
+                .map(this::toDTO)
+                .orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado com o ID: " + id));
+    }
 
 }
