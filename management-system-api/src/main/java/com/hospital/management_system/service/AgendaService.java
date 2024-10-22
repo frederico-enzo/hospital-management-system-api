120342d9773ca7c9f058fb5c5165b4279b995953
Frederico <frederico.enzo@hotmail.com>
2024-10-22 19:30:42 -0300

    feat metodos - read e deale para service Agenda

--- management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/AgendaService.java
@@ -1,8 +1,10 @@
 package com.hospital.management_system.service;
 
 import com.hospital.management_system.dto.AgendaDto;
+import com.hospital.management_system.dto.ConsultaDto;
 import com.hospital.management_system.model.Agenda;
 import com.hospital.management_system.repository.AgendaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +29,10 @@ public class AgendaService {
     public AgendaDto update(AgendaDto model){
         return toDTO(repository.save(toModel(model)));
     }
-
+    public void delete(Long id){ repository.deleteById(id);}
+    public AgendaDto read(Long id) {
+        return repository.findById(id)
+                .map(this::toDTO)
+                .orElseThrow(() -> new EntityNotFoundException("Agenda não encontrado com o ID: " + id));
+    }
 }
