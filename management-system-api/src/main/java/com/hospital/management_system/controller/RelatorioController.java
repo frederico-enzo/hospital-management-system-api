bba3ffb01c6399635d2330ba2375be8b18862bd7
Frederico <frederico.enzo@hotmail.com>
2024-10-21 21:12:19 -0300

    fix: relatorio repository e service o Autowired

--- management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
@@ -1,7 +1,11 @@
 package com.hospital.management_system.controller;
 
+import com.hospital.management_system.dto.AgendaDto;
+import com.hospital.management_system.dto.RelatorioDto;
 import com.hospital.management_system.repository.ProntuarioRepository;
+import com.hospital.management_system.repository.RelatorioRepository;
 import com.hospital.management_system.service.ProntuarioService;
+import com.hospital.management_system.service.RelatorioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +14,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/relatorio")
 public class RelatorioController {
     @Autowired
-    private ProntuarioService service;
+    private RelatorioService service;
     @Autowired
-    private ProntuarioRepository repository;
+    private RelatorioRepository repository;
 
     @GetMapping("/lista")
     public ResponseEntity<?> listarAll(){
         return ResponseEntity.ok(this.repository.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<RelatorioDto> create(@RequestBody RelatorioDto model) {
+        return ResponseEntity.ok(service.create(model));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<RelatorioDto> update(@RequestBody RelatorioDto model) {
+        return ResponseEntity.ok(service.update(model));
+
     }
 
 }
