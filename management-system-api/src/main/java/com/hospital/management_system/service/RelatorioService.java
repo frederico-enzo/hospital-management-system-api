8b820d6862e3d67fccae839b23e2eecc91caac43
Frederico <frederico.enzo@hotmail.com>
2024-10-22 19:33:42 -0300

    feat metodos - read e deale para service Relatorio

--- management-system-api/src/main/java/com/hospital/management_system/service/RelatorioService.java
+++ management-system-api/src/main/java/com/hospital/management_system/service/RelatorioService.java
@@ -3,6 +3,7 @@ package com.hospital.management_system.service;
 import com.hospital.management_system.dto.RelatorioDto;
 import com.hospital.management_system.model.Relatorio;
 import com.hospital.management_system.repository.RelatorioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,7 +24,15 @@ public class RelatorioService {
     public RelatorioDto create(RelatorioDto model){
         return toDTO(repository.save(toModel(model)));
     }
+    public RelatorioDto read(Long id) {
+        return repository.findById(id)
+                .map(this::toDTO)
+                .orElseThrow(() -> new EntityNotFoundException("Relatório não encontrado com o ID: " + id));
+    }
+
     public RelatorioDto update(RelatorioDto model){
         return toDTO(repository.save(toModel(model)));
     }
+    public void delete(Long id){ repository.deleteById(id);}
+
 }
