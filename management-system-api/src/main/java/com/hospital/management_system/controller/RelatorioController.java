505b8cd27f5995b47260b357218128ee32bcf885
Frederico <frederico.enzo@hotmail.com>
2024-10-23 16:30:39 -0300

    feat: metodos post-put-delete-get para controller Relatorio

--- management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
+++ management-system-api/src/main/java/com/hospital/management_system/controller/RelatorioController.java
@@ -1,14 +1,39 @@
 package com.hospital.management_system.controller;
 
 
+import com.hospital.management_system.dto.ProntuarioDto;
+import com.hospital.management_system.dto.RelatorioDto;
 import com.hospital.management_system.service.RelatorioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/relatorio")
 public class RelatorioController {
     @Autowired
     private RelatorioService service;
-
+    @GetMapping("/{id}")
+    public ResponseEntity<RelatorioDto> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<RelatorioDto>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<RelatorioDto> create(@RequestBody RelatorioDto model) {
+        return ResponseEntity.ok(service.post(model));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<RelatorioDto> put(@PathVariable Long id, @RequestBody RelatorioDto model) {
+        return ResponseEntity.ok(service.put(model,id));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
