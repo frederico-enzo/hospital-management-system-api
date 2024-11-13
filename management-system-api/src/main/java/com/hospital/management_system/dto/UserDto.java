6a2d6af54ef2b94cf771a0b38cd715a096b91ab7
Frederico <frederico.enzo@hotmail.com>
2024-11-12 21:20:19 -0300

    feat: user dto

--- management-system-api/src/main/java/com/hospital/management_system/dto/UserDto.java
+++ management-system-api/src/main/java/com/hospital/management_system/dto/UserDto.java
@@ -1,2 +1,18 @@
-package com.hospital.management_system.dto;public class UserDto {
+package com.hospital.management_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+    private String token;
+
+
 }
