7b76f195a117e9c416bc9e911ce8a32c0501a45d
Frederico <frederico.enzo@hotmail.com>
2024-11-12 21:20:46 -0300

    feat: Login repository

--- management-system-api/src/main/java/com/hospital/management_system/repository/LoginRepository.java
+++ management-system-api/src/main/java/com/hospital/management_system/repository/LoginRepository.java
@@ -1,2 +1,14 @@
-package com.hospital.management_system.repository;public interface LoginRepository {
+package com.hospital.management_system.repository;
+
+
+import com.hospital.management_system.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsername(String login);
+
 }
